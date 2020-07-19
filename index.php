e4f17ff954d36e84347ac7f0aa73f9213a20bcb1
AlexeyGod <forlem@gmail.com>
2020-07-19 20:15:31 +0300

    update composer

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
-/**
- * Created by Digital-Solution.Ru web-studio.
- * https://digital-solution.ru
- * support@digital-solution.ru
- */
 
 /* Настройки */
 $config = require(dirname(__FILE__).'/config/config.php');
 
 /* Автозагрузка классов */
-include 'autoload.php';
-Autoloader::register($config['autoLoader']);
-
-/* Установка */
-if(is_dir("install"))
-{
-	include "install/index.php";
-	exit();
-}
-
+include 'vendor/autoload.php';
 
 /* Старт приложения */
 $app = new framework\core\Application($config);
